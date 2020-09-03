564a089918b6d706f60543f6f88f75578c7befa8
PitzTech <victorlaurentino7@gmail.com>
2020-09-03 08:46:24 -0300

    03-09-2020.3

--- practice/2_Técnicas de Programação/Problemas Comuns/ArcoEflecha/ArcoEflecha.cpp
+++ practice/2_Técnicas de Programação/Problemas Comuns/ArcoEflecha/ArcoEflecha.cpp
@@ -1,28 +1,34 @@
 #include <bits/stdc++.h>
-#define INF INT_MAX
+#define INF LLONG_MAX
 #define pb push_back
 using namespace std;
+typedef long long ll;
 
-int mergeSort(vector<long>&v){
-    if(v.size() == 1) return 0;
+ll mergeSort(vector<ll>&v){
+	if(v.size() == 1) return 0;
 
-    int inter = 0;
-    vector<long> u1,u2;
-    for(int i = 0; i < v.size()/2;i++)
+    ll inter = 0;
+    vector<ll> u1,u2;
+
+	for(ll i = 0; i < v.size()/2;i++)
         u1.pb(v[i]);
-    for(int i = v.size()/2; i < v.size(); i++)
+    for(ll i = v.size()/2; i < v.size(); i++)
         u2.pb(v[i]);
 
     inter += mergeSort(u1);
     inter += mergeSort(u2);
 
-    int ini1 = 0, ini2 = 0;
-    for(int i = 0; i < v.size(); i++){
-        if(u1[ini1] <= u2[ini2]){
+    u1.pb(INF);
+    u2.pb(INF);
+
+	int ini1 = 0, ini2 = 0;
+    for(ll i = 0; i < v.size(); i++){
+        if(u1[ini1] <= u2[ini2])
             v[i] = u1[ini1++];
-            inter += u2.size() - ini2 - 1;
-        }else
+        else{
             v[i] = u2[ini2++];
+            inter += u1.size() - ini1 - 1;
+        }
     }
 
     return inter;
@@ -31,18 +37,18 @@ int mergeSort(vector<long>&v){
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(NULL);
-
-    int n;
-    cin >> n;
-
-    vector<long> v(n);
-    for(long &el : v){
-        int x, y;
-        cin >> x >> y;
-        el = x*x + y*y;
-    }
     
-    cout << mergeSort(v);
+    
+	ll n;
+	cin >> n;
 
-    return 0;
+    vector<ll> v(n);
+    for(ll &el : v){
+		ll x, y;
+		cin >> x >> y;
+        el = x*x + y*y;
+	}
+	cout<< (n*(n-1)/2) - mergeSort(v)<<endl;
+
+	return 0;
 }
